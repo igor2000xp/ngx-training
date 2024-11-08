cc25a824e0a58ff030545c37679c8ccfcb751fbd
igor2000xp <igorring@gmail.com>
2024-11-09 00:39:19 +0100

    feat: create first Plate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import {CALIFORNIA_PLATE, LICENSE_PLATES} from "./mock-data";
 import {LicensePlate} from "./license-plate";
-import { HelloComponent } from "./hello/hello.component";
+// import { HelloComponent } from "./hello/hello.component";
 import { NavigationComponent } from './navigation/navigation.component';
-import { JumbotronComponent } from './jumbotron/jumbotron.component';
+// import { JumbotronComponent } from './jumbotron/jumbotron.component';
 import { BindingsComponent } from './bindings/bindings.component';
+import { LicensePlateComponent } from './license-plate/license-plate.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavigationComponent, JumbotronComponent, BindingsComponent],
+  imports: [NavigationComponent, LicensePlateComponent, BindingsComponent],
   // imports: [HelloComponent],
   templateUrl: './app.component.html',
   // styleUrls: ['./app.component.scss']
