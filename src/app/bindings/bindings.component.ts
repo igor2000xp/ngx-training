c8a8f14c8275d1b2bb95c23e3a8327e5fecf5f12
igor2000xp <igorring@gmail.com>
2024-11-08 23:48:00 +0100

    feat: using ngModule and FormsModule

--- src/app/bindings/bindings.component.ts
+++ src/app/bindings/bindings.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-bindings',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './bindings.component.html',
   styleUrl: './bindings.component.css'
 })
