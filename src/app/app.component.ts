8a1c86b24f0160c21b2ee764ee03c56f29442e33
igor2000xp <igorring@gmail.com>
2024-11-08 14:22:41 +0100

    feat: add navigation bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 import {CALIFORNIA_PLATE, LICENSE_PLATES} from "./mock-data";
 import {LicensePlate} from "./license-plate";
 import {HelloComponent} from "./hello/hello.component";
+import { NavigationComponent } from './navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HelloComponent],
-  template: `
-      <app-hello></app-hello>
-  `
+  imports: [NavigationComponent],
+  // imports: [HelloComponent],
+  templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.scss']
+  // template: `
+  //     <app-hello></app-hello>
+  // `
 })
 export class AppComponent {
   licensePlates: LicensePlate[] = LICENSE_PLATES;
