33c63f48d8504266a236007c7a44b6647e2df8b2
igor2000xp <igorring@gmail.com>
2024-11-09 01:26:44 +0100

    feat: apply ngFor directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {CALIFORNIA_PLATE, LICENSE_PLATES} from "./mock-data";
 import {LicensePlate} from "./license-plate";
 // import { HelloComponent } from "./hello/hello.component";
@@ -7,11 +7,12 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { BindingsComponent } from './bindings/bindings.component';
 import { LicensePlateComponent } from './license-plate/license-plate.component';
 import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavigationComponent, JumbotronComponent, LicensePlateComponent, BindingsComponent],
+  imports: [NavigationComponent, JumbotronComponent, LicensePlateComponent, BindingsComponent, CommonModule],
   // imports: [HelloComponent],
   templateUrl: './app.component.html',
   // styleUrls: ['./app.component.scss']
@@ -19,7 +20,11 @@ import { JumbotronComponent } from './jumbotron/jumbotron.component';
   //     <app-hello></app-hello>
   // `
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+    console.log(this.licensePlates);
+  }
   licensePlates: LicensePlate[] = LICENSE_PLATES;
   licensePlate: LicensePlate = CALIFORNIA_PLATE;
 }
